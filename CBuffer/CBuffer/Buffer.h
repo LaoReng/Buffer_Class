c767be4c4f50f74ac8112e963039562b36eb0c7e
维东 鲁 <lwd15046543174@outlook.com>
2024-03-07 22:34:40 +0800

    完成任务:     1、 增加缓冲区容错机制，防止出现运行时崩溃     2、 增加数据更新方法(setData)     3、 新增虚构函数，防止出现内存泄露问题

--- CBuffer/CBuffer/Buffer.h
+++ CBuffer/CBuffer/Buffer.h
@@ -12,20 +12,39 @@ typedef unsigned char BYTE;
 typedef unsigned char BYTE;
 #endif // _MSC_VER
 
-
+// 缓冲区类
 class Buffer
 	:public std::string
 {
 public:
-	Buffer()
-		:std::string()
+	Buffer(size_t size = 0)
+		:std::string(size, '\0')
 	{}
+	Buffer(void* data, size_t size)
+		:std::string(size, '\0')
+	{
+		if (data != NULL) {
+			memcpy((void*)c_str(), data, size);
+		}
+	}
 	Buffer(const char* str)
 		:std::string(str)
 	{}
 	Buffer(const Buffer& buf)
 		:std::string(buf.c_str())
 	{}
+	virtual ~Buffer() {
+		clear();
+		std::string::~basic_string();
+	}
+	// 设置缓冲区数据
+	void SetData(const void* data, size_t size) {
+		clear();
+		resize(size);
+		if (data != NULL) {
+			memcpy((char*)c_str(), data, size);
+		}
+	}
 	Buffer& operator=(const Buffer& buf) {
 		if (this != &buf) {
 			std::string::operator=(buf.c_str());
